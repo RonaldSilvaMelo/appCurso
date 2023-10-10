ea6c43363c807c3c4894abda5d2618884a505d4e
RonaldSilvaMelo <wolfyrony12@gmail.com>
2023-10-09 21:58:32 -0300

    Segunda versão

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class CategoriaController extends Controller
 {
-    //
+    //abre o formulario de cadastro da categoria
+    public function mostrarFormCat(){
+        return view('cad_categoria');
+    }
+
+    //abre a home
+    public function index(){
+        return view('index');
+    }
 }
